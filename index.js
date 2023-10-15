5ff40e25673e7525908fedb7858c88f8baaba99c
Michael3194 <143271230+Michael3194@users.noreply.github.com>
2023-10-15 20:27:25 -0230

    change filePath from const to let

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs'); // Import filesystem module
 // Create an HTTP server
 const server = http.createServer((request, response) => {
 
-    const filePath = './views/' // Set the default file path
+    let filePath = './views/' // Set the default file path
     const urlPath = request.url; // Get the URL path
 
     // Map the URL paths to the file paths
