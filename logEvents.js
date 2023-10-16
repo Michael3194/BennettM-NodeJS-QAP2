f78731006345943192da643c89920ce33cce9b7f
Michael3194 <143271230+Michael3194@users.noreply.github.com>
2023-10-16 11:39:55 -0230

    created a new logEvents.js file with logEvents fnc

---
+++ logEvents.js
@@ -0,0 +1,38 @@
+const fs = require('fs'); // Import the filesystem module
+const fsPromises = require('fs').promises; // Import promises functionality for fs module
+const path = require('path') // Import the path module
+
+const { format } = require('date-fns'); // Import the date-fns module to format dates
+
+const { v4: uuidv4 } = require('uuid'); // Import the uuid module to generate unique IDs
+
+const logEvents = async (message) => {
+
+    // Format the current date and time
+    const dateTime = `${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`;
+
+    // This is the format for each log item in the log file
+    const logItem = `${dateTime}\t${message}\t${uuidv4()}`;
+
+    try {
+        // If log directory does not exist
+        if (!fs.existsSync(path.join(__dirname, 'logs'))) {
+
+            // Then create it here
+            await fsPromises.mkdir(path.join(__dirname, 'logs')); 
+        }
+
+        // Create the log file name
+        const fileName = `${format(new Date(), 'yyyyMMdd')}` + '_serverEvents.log';
+
+        // Append the log item to the log file
+        await fsPromises.appendFile(path.join(__dirname, 'logs', fileName), logItem + '\n');
+
+    // Catch any errors
+    } catch (err) {
+        console.log(err) // And log them to the console
+    }
+
+}
+
+module.exports = { myEmitter, logEvents }
